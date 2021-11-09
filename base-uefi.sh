554922e9b83e8430c88a965edf71c55ab1483af9
jgarret1 <jgarret1@gmail.com>
2021-11-09 12:05:11 -0300

    customizacion final

--- base-uefi.sh
+++ base-uefi.sh
@@ -15,31 +15,23 @@ echo root:password | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S grub efibootmgr intel-ucode networkmanager network-manager-applet dialog mtools dosfstools reflector linux-headers xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils bash-completion openssh rsync reflector os-prober ntfs-3g terminus-font
 
-# pacman -S --noconfirm xf86-video-amdgpu
+pacman -S --noconfirm xf86-video-intel
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable --recheck 
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable cups.service
-systemctl enable sshd
-systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-systemctl enable reflector.timer
-systemctl enable fstrim.timer
-systemctl enable libvirtd
-systemctl enable firewalld
-systemctl enable acpid
+# systemctl enable sshd
 
-useradd -m ermanno
-echo ermanno:password | chpasswd
-usermod -aG libvirt ermanno
+useradd -mG wheel jgl
+echo jgl:password | chpasswd
 
-echo "ermanno ALL=(ALL) ALL" >> /etc/sudoers.d/ermanno
+# echo "ermanno ALL=(ALL) ALL" >> /etc/sudoers.d/ermanno
 
 
 printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
